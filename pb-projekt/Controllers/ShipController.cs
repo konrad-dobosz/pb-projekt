13b36e52baf68c76aa78ebfbfd4e1914e67f1cb8
damianbalon <damian.ba7@gmail.com>
2024-06-03 13:08:32 +0200

    Adding ShipController and Ship View

---
+++ pb-projekt/Controllers/ShipController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using pb_projekt.Data;
+using pb_projekt.Models;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace pb_projekt.Controllers
+{
+    public class ShipController : Controller
+    {
+        private readonly AppDbContext _context;
+
+        public ShipController(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        [Route("/ships", Name = "Ships")]
+        public async Task<IActionResult> Index()
+        {
+            var ships = await _context.Ships.ToListAsync();
+            return View(ships);
+        }
+    }
+}
--- pb-projekt/Program.cs
+++ pb-projekt/Program.cs
@@ -10,25 +10,46 @@ var connectionString = builder.Configuration.GetConnectionString("DefaultConnect
 builder.Services.AddDbContext<AppDbContext>(options => options.UseMySQL(connectionString));
 
 builder.Services.AddDefaultIdentity<User>(options =>
-    {
-        options.SignIn.RequireConfirmedAccount = false;
-        options.Password.RequiredUniqueChars = 0;
-        options.Password.RequireDigit = false;
-        options.Password.RequireUppercase = false;
-        options.Password.RequiredLength = 1;
-        options.Password.RequireLowercase = false;
-        options.Password.RequireNonAlphanumeric = false;
-    })
+{
+    options.SignIn.RequireConfirmedAccount = false;
+    options.Password.RequiredUniqueChars = 0;
+    options.Password.RequireDigit = false;
+    options.Password.RequireUppercase = false;
+    options.Password.RequiredLength = 1;
+    options.Password.RequireLowercase = false;
+    options.Password.RequireNonAlphanumeric = false;
+})
     .AddEntityFrameworkStores<AppDbContext>();
 
 var app = builder.Build();
 
-//app.UseHttpsRedirection();  
+using (var scope = app.Services.CreateScope())
+{
+    var services = scope.ServiceProvider;
+    var dbContext = services.GetRequiredService<AppDbContext>();
+
+    // Dodaj przyk³adowe statki do bazy danych
+    if (!dbContext.Ships.Any())
+    {
+        dbContext.Ships.AddRange(
+            new Ship { CargoCapacity = 10000, DockingSpace = "Dock A" },
+            new Ship { CargoCapacity = 15000, DockingSpace = "Dock B" },
+            new Ship { CargoCapacity = 20000, DockingSpace = "Dock C" }
+        );
+        dbContext.SaveChanges();
+    }
+}
+
 app.UseStaticFiles();
 
 app.UseRouting();
 
 app.UseAuthorization();
+
+app.MapControllerRoute(
+    name: "ships",
+    pattern: "ships",
+    defaults: new { controller = "Ship", action = "Index" });
 
 app.MapControllerRoute(
     name: "default",
