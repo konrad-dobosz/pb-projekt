46459bde00b35a1fbd9bb9135428375c3f22bf10
konrad-dobosz <crisps.taffies.0w@icloud.com>
2024-06-24 16:17:19 +0200

    Change texts and clean up the home controller

--- pb-projekt/Controllers/HomeController.cs
+++ pb-projekt/Controllers/HomeController.cs
@@ -1,41 +1,14 @@
     using Microsoft.AspNetCore.Mvc;
-    using Microsoft.EntityFrameworkCore;
-    using pb_projekt.Data;
-    using pb_projekt.Models;
-    using System.Threading.Tasks;
 
     namespace pb_projekt.Controllers
     {
         public class HomeController : Controller
         {
-            private readonly AppDbContext _appDbContext;
-
-            public HomeController(AppDbContext appDbContext)
-            {
-                _appDbContext = appDbContext;
-            }
-
             [HttpGet]
             [Route("/", Name = "Home")]
             public IActionResult Index()
             {
-                var ships = _appDbContext.Ships.ToList();
-                return View(ships);
-            }
-
-            [HttpGet]
-            public async Task<IActionResult> Details(int id)
-            {
-                var ship = await _appDbContext.Ships
-                    .Include(s => s.Cargoes) // Eagerly load the related cargoes
-                    .FirstOrDefaultAsync(s => s.Id == id);
-
-                if (ship == null)
-                {
-                    return NotFound();
-                }
-
-                return View(ship);
+                return RedirectToRoute("Ships");
             }
         }
     }
